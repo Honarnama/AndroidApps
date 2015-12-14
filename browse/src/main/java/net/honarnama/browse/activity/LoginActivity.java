168fcd2f534a3a2547e2e4cf622bccdf265b877e
Elnaz Mojahedi <mojahedi.elnaz@gmail.com>
2015-12-15 01:10:50 +0330

    gender toggle buttons added to register form

--- browse/src/main/java/net/honarnama/browse/activity/LoginActivity.java
+++ browse/src/main/java/net/honarnama/browse/activity/LoginActivity.java
@@ -3,22 +3,35 @@ package net.honarnama.browse.activity;
 import net.honarnama.browse.R;
 import net.honarnama.core.activity.HonarnamaBaseActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by elnaz on 12/13/15.
  */
-public class LoginActivity extends HonarnamaBaseActivity implements View.OnClickListener{
+public class LoginActivity extends HonarnamaBaseActivity implements View.OnClickListener {
 
+    TextView mRegisterAsCustomerTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mRegisterAsCustomerTextView = (TextView) findViewById(R.id.register_as_customer_text_view);
+        mRegisterAsCustomerTextView.setOnClickListener(this);
     }
 
     @Override
-    public void onClick(View v) {
+    public void onClick(View view) {
 
+        switch (view.getId()) {
+            case R.id.register_as_customer_text_view:
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
